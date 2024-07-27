ad5657b37f0845604d2b87d150698fb82475206c
rromero123102 <rromero21332@columbushs.com>
2024-07-27 13:25:12 -0400

    Add project files.

---
+++ AssemblyInfo1.cs
@@ -0,0 +1,5 @@
+using MelonLoader;
+using SlapshotCustomServers;
+
+[assembly: MelonInfo(typeof(MainMelon), "Mod Name", "1.0.0", "Your name")]
+[assembly: MelonGame("Oddshot Games", "Slapshot")] //this line must be exactly like this
---
+++ MainMelon.cs
@@ -0,0 +1,88 @@
+﻿using MelonLoader;
+using LiteNetLib;
+using LiteNetLib.Utils;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace SlapshotCustomServers
+{
+    public class MainMelon : MelonMod
+    {
+
+    }
+    public class Server : INetEventListener
+    {
+        public void OnConnectionRequest(ConnectionRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNetworkError(IPEndPoint endPoint, SocketError socketError)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNetworkLatencyUpdate(NetPeer peer, int latency)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNetworkReceive(NetPeer peer, NetPacketReader reader, byte channelNumber, DeliveryMethod deliveryMethod)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNetworkReceiveUnconnected(IPEndPoint remoteEndPoint, NetPacketReader reader, UnconnectedMessageType messageType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnPeerConnected(NetPeer peer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnPeerDisconnected(NetPeer peer, DisconnectInfo disconnectInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Client : INetEventListener
+    {
+        public void OnConnectionRequest(ConnectionRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNetworkError(IPEndPoint endPoint, SocketError socketError)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNetworkLatencyUpdate(NetPeer peer, int latency)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNetworkReceive(NetPeer peer, NetPacketReader reader, byte channelNumber, DeliveryMethod deliveryMethod)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNetworkReceiveUnconnected(IPEndPoint remoteEndPoint, NetPacketReader reader, UnconnectedMessageType messageType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnPeerConnected(NetPeer peer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnPeerDisconnected(NetPeer peer, DisconnectInfo disconnectInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
